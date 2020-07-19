f83acee0fe17828c0465f0dd3e6e9dcff39fce0f
ehazony <efraimhazony@gmail.com>
2020-07-20 00:00:03 +0300

    changes for server appache

--- finance/wsgi.py
+++ finance/wsgi.py
@@ -7,8 +7,13 @@ For more information on this file, see
 https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
 """
 
-import os
 
+import os
+import sys
+
+sys.path.append('/opt/bitnami/apps/django/django_projects/finance')
+
+os.environ.setdefault("PYTHON_EGG_CACHE", "/opt/bitnami/apps/django/django_projects/finance/egg_cache")
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'finance.settings')
