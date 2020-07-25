c27c28c5090edba77508df3ba018627892766d7c
ehazony <efraimhazony@gmail.com>
2020-07-26 02:00:01 +0300

    changes for server appache

--- authentication/urls.py
+++ authentication/urls.py
@@ -6,7 +6,7 @@ Copyright (c) 2019 - present AppSeed.us
 from django.conf.urls import url
 from django.urls import path
 
-from rest_framework.authtoken import views
+
 from .views import login_view, register_user
 from django.contrib.auth.views import LogoutView
 
@@ -14,7 +14,4 @@ urlpatterns = [
     path('login/', login_view, name="login"),
     path('register/', register_user, name="register"),
     path("logout/", LogoutView.as_view(), name="logout")
-]
-urlpatterns += [
-    url(r'^api-token-auth/', views.obtain_auth_token)
 ]
