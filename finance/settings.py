0671b57dadb93fffe78c5f83b7ab55b69475c999
Efraim Hazony <efraim@furius.com>
2020-07-31 01:57:21 +0300

    changes for server appache

--- finance/settings.py
+++ finance/settings.py
@@ -123,7 +123,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-# STATIC_URL = '/core/static/'
+STATIC_URL = '/staticfiles/'
 #
 #
 STATICFILES_DIRS = [
