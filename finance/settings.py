597cb104cf5ce030973f9349cd61807b4d2705e3
Efraim Hazony <efraim@furius.com>
2020-08-02 00:20:38 +0300

    hope this works

--- finance/settings.py
+++ finance/settings.py
@@ -127,7 +127,7 @@ STATIC_URL = '/static/'
 #
 #
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
+    # os.path.join(BASE_DIR, "static"),
 ]
 
 LOGIN_REDIRECT_URL = "home"  # Route defined in app/urls.py
