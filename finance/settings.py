3eb8121de181c1b4be8dc3d5fc8f37122ee1e406
Efraim Hazony <efraim@furius.com>
2022-01-06 14:46:48 +0200

    logging

--- finance/settings.py
+++ finance/settings.py
@@ -138,3 +138,29 @@ STATICFILES_DIRS = [
 LOGIN_REDIRECT_URL = "home"  # Route defined in app/urls.py
 LOGOUT_REDIRECT_URL = "home"  # Route defined in app/urls.py
 django_heroku.settings(locals())
+
+
+
+LOGGING = {
+	'version': 1,
+	# The version number of our log
+	'disable_existing_loggers': False,
+	# django uses some of its own loggers for internal operations. In case you want to disable them just replace the False above with true.
+	# A handler for WARNING. It is basically writing the WARNING messages into a file called WARNING.log
+	'handlers': {
+		'file': {
+			'level': 'WARNING',
+			'class': 'logging.FileHandler',
+			'filename': os.path.join(BASE_DIR,  'file.log'),
+		},
+	},
+	# A logger for WARNING which has a handler called 'file'. A logger can have multiple handler
+	'loggers': {
+		# notice the blank '', Usually you would put built in loggers like django or root here based on your needs
+		'': {
+			'handlers': ['file'], #notice how file variable is called in handler which has been defined above
+			'level': 'DEBUG',
+			'propagate': True,
+		},
+	},
+}
