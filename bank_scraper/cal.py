9c6a58e527d5fa55e9f46fb7c3c073d35e9038ad
Efraim Hazony <efraimhazony@gmail.com>
2022-10-11 13:47:31 +0300

    cal scraper fix

--- bank_scraper/cal.py
+++ bank_scraper/cal.py
@@ -1,12 +1,15 @@
 import datetime
 import os
 import time
+
 import django
-from selenium.webdriver.support.wait import WebDriverWait
 import selenium.webdriver.support.expected_conditions as EC
+from selenium.webdriver.support.wait import WebDriverWait
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "finance.settings")
 django.setup()
 from selenium.webdriver.common.by import By
+from dateutil import relativedelta
 
 from bank_scraper.selenium_api import get_selenium_driver
 from bank_scraper.base_scraper import Scraper
@@ -17,19 +20,15 @@ class CalScraper(Scraper):
     def __init__(self):
         self.COMPANY = 'CAL'
 
-    def get_transactions(self, start, end, credential, username=None, password=None, grid=True,*args, **kwargs):
+    def get_transactions(self, start, end, credential, username=None, password=None, grid=True, *args, **kwargs):
         # options = uc.ChromeOptions()
         # options.headless = True
         # options.add_argument('--headless')
         # driver = uc.Chrome(options=options)
-        driver = get_selenium_driver(grid=False, headless=False)
+        driver = get_selenium_driver(grid=grid)
         try:
-            start_year_month = start.strftime('%m%Y')
-            end_year_month = start.strftime('%m%Y')
-            start_day = start.strftime('%-d')
-            end_day = end.strftime('%-d')
 
-        # driver = uc.Chrome()
+            # driver = uc.Chrome()
 
             driver.get('https://www.cal-online.co.il/')
             time.sleep(0.7)
@@ -46,9 +45,13 @@ class CalScraper(Scraper):
             driver.find_elements(By.XPATH, "//button[contains(., ' כניסה ')]")[0].click()
             # time.sleep(10)
             # driver.add_cdp_listener('Network.responseReceived', mylousyprintfunction)
-            next_debit_sum = WebDriverWait(driver, 60).until(EC.visibility_of_element_located((By.ID, "lblNextDebitSum"))).text
+            next_debit_sum = WebDriverWait(driver, 60).until(
+                EC.visibility_of_element_located((By.ID, "lblNextDebitSum"))).text
+            next_bill_date_str = WebDriverWait(driver, 60).until(
+                EC.visibility_of_element_located((By.ID, "lblNextDebitDate"))).text
+            next_bill_date = datetime.datetime.strptime(next_bill_date_str, '%d/%m/%Y')
             # next_debit_sum = driver.find_element(By.ID, 'lblNextDebitSum').text
-            credential.additional_info[credential.ADDITIONAL_INFO_BALANCE] = float(next_debit_sum)*-1
+            credential.additional_info[credential.ADDITIONAL_INFO_BALANCE] = float(next_debit_sum) * -1
             credential.save()
             driver.get('https://services.cal-online.co.il/Card-Holders/SCREENS/Transactions/Transactions.aspx')
             driver.find_element(By.XPATH,
@@ -59,74 +62,110 @@ class CalScraper(Scraper):
             driver.find_element(By.XPATH,
                                 '//*[@id="ctl00_ContentTop_cboCardList_categoryList_updatePanelList"]/table/tbody/tr/td[1]/div/span[2]/div[5]/div').click()
 
-            driver.find_element(By.ID, 'ctl00_FormAreaNoBorder_FormArea_rdoTransactionDate').click()
-            time.sleep(2)
-            # Start
-            driver.find_element(By.ID, 'ctl00_FormAreaNoBorder_FormArea_ctlDateScopeStart_ctlMonthYearList_Button').click()
-            time.sleep(1)
-            driver.find_element(By.ID,
-                                'ctl00_FormAreaNoBorder_FormArea_ctlDateScopeStart_ctlMonthYearList_OptionList').find_element(
-                By.XPATH, 'li[@value="{}"]'.format(start_year_month)).click()
-
-            driver.find_element(By.ID, 'ctl00_FormAreaNoBorder_FormArea_ctlDateScopeStart_ctlDaysList_Button').click()
-            time.sleep(1)
-            driver.find_element(By.ID,
-                                'ctl00_FormAreaNoBorder_FormArea_ctlDateScopeStart_ctlDaysList_OptionList').find_element(
-                By.XPATH, 'li[@value="{}"]'.format(start_day)).click()
-            # End
-            driver.find_element(By.ID, 'ctl00_FormAreaNoBorder_FormArea_ctlDateScopeEnd_ctlMonthYearList_Button').click()
-            time.sleep(1)
-            driver.find_element(By.ID,
-                                'ctl00_FormAreaNoBorder_FormArea_ctlDateScopeEnd_ctlMonthYearList_OptionList').find_element(
-                By.XPATH, 'li[@value="{}"]'.format(end_year_month)).click()
-
-            driver.find_element(By.ID, 'ctl00_FormAreaNoBorder_FormArea_ctlDateScopeEnd_ctlDaysList_Button').click()
-            time.sleep(1)
-            driver.find_element(By.ID,
-                                'ctl00_FormAreaNoBorder_FormArea_ctlDateScopeEnd_ctlDaysList_OptionList').find_element(
-                By.XPATH, 'li[@value="{}"]'.format(end_day)).click()
-
-        # הצג
+            months = self._get_months_to_scrape(start, end, next_bill_date)
             transactions = []
-            for i, card in enumerate(cards):
-                driver.find_element(By.XPATH,
-                                    '//*[@id="ctl00_ContentTop_cboCardList_categoryList_updatePanelList"]/table/tbody/tr/td[1]/div/span[2]/div[5]/div').click()
-                time.sleep(2)
+            for month in months:
+                temp_current_bill_date = datetime.datetime.strptime(month, '%m%Y')
+                temp_current_last_bill_date = temp_current_bill_date - relativedelta.relativedelta(months=1)
+                temp_bill_day = temp_current_bill_date.day
+                self._set_search_month_by_date_billed(driver, month)
+                for i in range(len(cards)):
+                    current_transactions = []
+                    driver.find_element(By.XPATH,
+                                        '//*[@id="ctl00_ContentTop_cboCardList_categoryList_updatePanelList"]/table/tbody/tr/td[1]/div/span[2]/div[5]/div').click()
+                    time.sleep(2)
 
-                driver.find_element(By.ID, 'ctl00_ContentTop_cboCardList_categoryList_pnlMain').find_elements(
-                    By.CSS_SELECTOR,
-                    'a')[i].click()
-                time.sleep(1)
-                driver.find_element(By.ID, 'ctl00_FormAreaNoBorder_FormArea_ctlSubmitRequest').click()
+                    driver.find_element(By.ID, 'ctl00_ContentTop_cboCardList_categoryList_pnlMain').find_elements(
+                        By.CSS_SELECTOR,
+                        'a')[i].click()
+                    time.sleep(1)
+                    driver.find_element(By.ID, 'ctl00_FormAreaNoBorder_FormArea_ctlSubmitRequest').click()
 
-                time.sleep(5)
-
-                trs = driver.find_elements(By.XPATH, '//*[@id="ctlMainGrid"]/tbody[1]/tr')
-                for tr in trs:
-                    tds = tr.find_elements(By.XPATH, 'td')
-                    values = [td.text for td in tds]
-                    values = dict(zip(['date', 'name', 'value_total', 'value', 'info'], values))
-
-                # go through pages
-                while len(driver.find_elements(By.ID, 'ctl00_FormAreaNoBorder_FormArea_ctlGridPager_btnNext')) != 0:
-                    driver.find_element(By.ID, 'ctl00_FormAreaNoBorder_FormArea_ctlGridPager_btnNext').click()
                     time.sleep(5)
+
                     trs = driver.find_elements(By.XPATH, '//*[@id="ctlMainGrid"]/tbody[1]/tr')
+
+                    # first page
                     for tr in trs:
                         tds = tr.find_elements(By.XPATH, 'td')
                         values = [td.text for td in tds]
                         values = dict(zip(['date', 'name', 'value_total', 'value', 'info'], values))
-                        transactions.append(values)
+                        current_transactions.append(values)
 
-            for trans in transactions:
-                value = trans['value'].replace('₪ ', '').replace(',', '')
-                value_total = trans['value_total'].replace('₪ ', '').replace(',', '')
-                trans['amount'] = float(value) if value else None
-                trans['value_total'] = float(value_total) if value_total else None
-                trans['date'] = datetime.datetime.strptime(trans['date'], '%d/%m/%y')
+                    # go through pages
+                    while len(driver.find_elements(By.ID, 'ctl00_FormAreaNoBorder_FormArea_ctlGridPager_btnNext')) != 0:
+                        driver.find_element(By.ID, 'ctl00_FormAreaNoBorder_FormArea_ctlGridPager_btnNext').click()
+                        time.sleep(5)
+                        trs = driver.find_elements(By.XPATH, '//*[@id="ctlMainGrid"]/tbody[1]/tr')
+                        for tr in trs:
+                            tds = tr.find_elements(By.XPATH, 'td')
+                            values = [td.text for td in tds]
+                            values = dict(zip(['date', 'name', 'value_total', 'value', 'info'], values))
+                            current_transactions.append(values)
+
+                    # format data
+                    for trans in current_transactions:
+                        value = trans['value'].replace('₪ ', '').replace(',', '')
+                        value_total = trans['value_total'].replace('₪ ', '').replace(',', '')
+                        trans['amount'] = float(value) if value else None
+                        trans['deal_total'] = float(value_total) if value_total and '$' not in value_total else None
+                        trans['date'] = datetime.datetime.strptime(trans['date'], '%d/%m/%y')
+                        # set date to be in this month billing period
+                        if not temp_current_last_bill_date <= trans['date'] <= temp_current_bill_date:
+                            trans['date'] = temp_current_last_bill_date.replace(day=trans['date'].day) \
+                                if temp_bill_day <= trans['date'].day\
+                                else temp_current_bill_date.replace(day=trans['date'].day)
+
+                    transactions.extend(current_transactions)
         except Exception as e:
             driver.quit()
             raise e
         driver.quit()
-        return transactions
+        return filter(lambda t: start <= t['date'] <= end, transactions)
 
+    def _get_months_to_scrape(self, start, end, bill_date):
+        months = [str(bill_date.month) + str(bill_date.year)]
+        previous_bill_date = bill_date - relativedelta.relativedelta(months=1)
+        while start < previous_bill_date:
+            months.append(previous_bill_date.strftime('%m%Y'))
+            previous_bill_date = previous_bill_date - relativedelta.relativedelta(months=1)
+        return months
+
+    def _set_search_month_by_date_billed(self, driver, month):
+        driver.find_element(By.ID, 'ctl00_FormAreaNoBorder_FormArea_clndrDebitDateScope_Button').click()  # open picker
+        driver.find_element(By.ID,  # select month date
+                            'ctl00_FormAreaNoBorder_FormArea_clndrDebitDateScope_OptionList').find_element(
+            By.XPATH, 'li[@value="{}"]'.format(month)).click()
+
+    def _set_search_date_by_date_transaction_created(self, driver, start, end):
+        start_year_month = start.strftime('%m%Y')
+        end_year_month = start.strftime('%m%Y')
+        start_day = start.strftime('%-d')
+        end_day = end.strftime('%-d')
+
+        driver.find_element(By.ID, 'ctl00_FormAreaNoBorder_FormArea_rdoTransactionDate').click()
+        time.sleep(2)
+        # Start
+        driver.find_element(By.ID, 'ctl00_FormAreaNoBorder_FormArea_ctlDateScopeStart_ctlMonthYearList_Button').click()
+        time.sleep(1)
+        driver.find_element(By.ID,
+                            'ctl00_FormAreaNoBorder_FormArea_ctlDateScopeStart_ctlMonthYearList_OptionList').find_element(
+            By.XPATH, 'li[@value="{}"]'.format(start_year_month)).click()
+
+        driver.find_element(By.ID, 'ctl00_FormAreaNoBorder_FormArea_ctlDateScopeStart_ctlDaysList_Button').click()
+        time.sleep(1)
+        driver.find_element(By.ID,
+                            'ctl00_FormAreaNoBorder_FormArea_ctlDateScopeStart_ctlDaysList_OptionList').find_element(
+            By.XPATH, 'li[@value="{}"]'.format(start_day)).click()
+        # End
+        driver.find_element(By.ID, 'ctl00_FormAreaNoBorder_FormArea_ctlDateScopeEnd_ctlMonthYearList_Button').click()
+        time.sleep(1)
+        driver.find_element(By.ID,
+                            'ctl00_FormAreaNoBorder_FormArea_ctlDateScopeEnd_ctlMonthYearList_OptionList').find_element(
+            By.XPATH, 'li[@value="{}"]'.format(end_year_month)).click()
+
+        driver.find_element(By.ID, 'ctl00_FormAreaNoBorder_FormArea_ctlDateScopeEnd_ctlDaysList_Button').click()
+        time.sleep(1)
+        driver.find_element(By.ID,
+                            'ctl00_FormAreaNoBorder_FormArea_ctlDateScopeEnd_ctlDaysList_OptionList').find_element(
+            By.XPATH, 'li[@value="{}"]'.format(end_day)).click()
